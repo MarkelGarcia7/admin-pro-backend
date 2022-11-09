9c31f2cb67a6037013ecf5f1708f889471540800
Markel Garcia <markel.garcia@entelgy-ibai.com>
2022-11-09 13:14:08 +0100

    Sección 12 - Google

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const bcryptjs = require('bcryptjs')
 
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async( req, res = response ) => {
 
@@ -46,10 +47,54 @@ const login = async( req, res = response ) => {
             msg: 'Hable con el administrador'
         });
     }
+}
+
+const googleSignIn = async( req, res = response ) => {
+
+    try {
+        
+        const { email, name, picture } = await googleVerify( req.body.token );
+
+        const usuarioDB = await Usuario.findOne({ email });
+        let usuario;
+
+        if ( !usuarioDB ) {
+            usuario = new Usuario({
+                nombre: name,
+                email,
+                password: '@@@',
+                img: picture,
+                google: true
+            })
+        } else {
+            usuario = usuarioDB;
+            usuario.google = true;
+        }
+
+        /* guardar usuario en BD */
+        await usuario.save();
+
+        /* Generar el token - JsonWebToken */
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            ok: true,
+            email, name, picture,
+            token
+        })
+
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({
+            ok: false,
+            msg: 'Token de google no es correcto'
+        });
+    }   
 
 }
 
 
 module.exports = {
-    login 
+    login,
+    googleSignIn 
 }
