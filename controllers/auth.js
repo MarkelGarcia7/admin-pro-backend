ffe0f3fc4f8ff6ec33fd7260c7b4a45c410ca954
Markel Garcia <markel.garcia@entelgy-ibai.com>
2022-11-28 12:32:42 +0100

    Fin seccion 18

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,7 @@ const bcryptjs = require('bcryptjs')
 const Usuario = require('../models/usuario');
 const { generarJWT } = require('../helpers/jwt');
 const { googleVerify } = require('../helpers/google-verify');
+const { getMenuFrontEnd } = require('../helpers/menu-frontend')
 
 const login = async( req, res = response ) => {
 
@@ -36,7 +37,8 @@ const login = async( req, res = response ) => {
 
         res.json({
             ok: true,
-            token
+            token,
+            menu: getMenuFrontEnd( usuarioDB.role )
         })
         
     } catch (error) {
@@ -80,7 +82,8 @@ const googleSignIn = async( req, res = response ) => {
         res.json({
             ok: true,
             email, name, picture,
-            token
+            token,
+            menu: getMenuFrontEnd( usuario.role )
         })
 
     } catch (error) {
@@ -105,7 +108,8 @@ const renewToken = async ( req, res = response ) => {
     res.json({
         ok: true,
         token,
-        usuario
+        usuario,
+        menu: getMenuFrontEnd( usuario.role )
     })
 
 }
